354bb6568d1d9e1da248922c8255b47992ae47fc
ThreeFx <ben@sjfiedler.de>
2015-03-16 17:48:54 +0100

    Implementation now constucts suffix trees successfully

--- missisippi/mississippi.pas
+++ missisippi/mississippi.pas
@@ -5,12 +5,14 @@ PROGRAM Mississippi;
 USES SuffixTree;
 
 VAR
- 	tree : TSuffixTree;
+	tree : TSuffixTree;
+	s : String;
 
 BEGIN
-
-  tree := CreateSuffixTreeFrom('abaaba');
-  Write('Press any key to exit...');
-  Read();
-
+	s := 'abaaba';
+	WriteLn('Creating suffix tree for: ', s);
+	tree := CreateSuffixTreeFrom(s);
+	WriteLn('Created suffix tree with ', tree.nodes, ' nodes.');
+	Write('Press enter to exit...');
+	ReadLn;
 END.
--- missisippi/suffixtree.pas
+++ missisippi/suffixtree.pas
@@ -2,110 +2,176 @@ UNIT SuffixTree;
 
 INTERFACE
 
-  TYPE
-    Nptr = ^TNode;
-  
-    TNode = RECORD
-      child        : Nptr;
-      next_sibling : Nptr;
-      offset       : Integer;
-      len          : Integer;
-    END;
-  
-    TSuffixTree = RECORD
-      root : TNode;
-    END;
+	TYPE
+		Nptr = ^TNode;
+	
+		TNode = RECORD
+			offset       : Integer;
+			len	         : Integer;
+			next_sibling : Nptr;
+			child        : Nptr;
+		END;
+	
+		TSuffixTree = RECORD
+			root : TNode;
+			s    : String;
+			nodes : Integer;
+		END;
 
-  FUNCTION CreateSuffixTreeFrom(s : String) : TSuffixTree;
+	FUNCTION CreateSuffixTreeFrom(s : String) : TSuffixTree;
+	FUNCTION GetString(node : TNode; s : String) : String;
+	FUNCTION NumberLeaves(tree : TSuffixTree) : Integer;
 
 IMPLEMENTATION
 
-  FUNCTION GetNodeAt(node : Nptr; s : String; c : Char) : Nptr;
-  VAR
-    result : Nptr;
-  BEGIN
-    result := nil;
-    WHILE node <> nil DO
-    BEGIN
-      IF s[node^.offset + 1] = c THEN
-      BEGIN
-        result := node;
-        exit;
-      END;
-      node := node^.next_sibling;
-    END;
-  END;
+	FUNCTION GetNodeAt(node : Nptr; s : String; c : Char) : Nptr;
+	{VAR
+		result : Nptr;}
+	BEGIN
+		result := nil;
+		WHILE node <> nil DO
+		BEGIN
+			IF s[node^.offset + 1] = c THEN
+			BEGIN
+				result := node;
+				exit;
+			END;
+			node := node^.next_sibling;
+		END;
+	END;
 
-  FUNCTION CreateSuffixTreeFrom(s : String) : TSuffixTree;
-  VAR
-    i, j, k, len, offset : Integer;
-    cur, child, mid, newNode : Nptr;
-    root : TNode;
-    result : TSuffixTree;
-  BEGIN
-    s := s + '$';
+	FUNCTION CreateSuffixTreeFrom(s : String) : TSuffixTree;
+	VAR
+		i, j, k, len, offset, nodes : Integer;
+		cur, child, mid, newNode : Nptr;
+		root : TNode;
+		{result : TSuffixTree;}
+	BEGIN
+		s := s + '$';
 
-    root.offset := 0;
-    root.len := Length(s);
-    root.next_sibling := nil;
-    root.child := nil;
+		cur := GetMem(SizeOf(TNode));
+		cur^.offset := 0;
+		cur^.len := Length(s);
+		cur^.next_sibling := nil;
+		cur^.child := nil;
 
-    FOR i := 2 TO Length(s) DO
-    BEGIN
-      cur := Addr(root);
-      j := i;
+		root.offset := 0;
+		root.len := 0;
+		root.next_sibling := nil;
+		root.child := cur;
 
-      WHILE j < Length(s) DO
-      BEGIN
-        child := GetNodeAt(cur^.child, s, s[j]);
+		nodes := 2;
 
-        IF child <> nil THEN
-        BEGIN
-          offset := child^.offset;
-          len := child^.len;
+		WriteLn('Root node: ',GetString(cur^, s));
 
-          k := j + 1;
-          WHILE (k - j < len) AND (s[k] = s[offset + k - j + 1]) DO
-          BEGIN
-            Inc(k);
-          END;
+		FOR i := 2 TO Length(s) DO
+		BEGIN
+			WriteLn('Iteration #',i - 1);
+			cur := Addr(root);
+			j := i;
 
-          IF k - j = len THEN
-          BEGIN
-            cur := child;
-            j := k;
-          END
-          ELSE
-          BEGIN
-            newNode := GetMem(SizeOf(TNode));
-            newNode^.offset := k;
-            newNode^.len := Length(s) - k;
-            newNode^.next_sibling := cur^.child;
-            newNode^.child := nil;
+			WHILE j <= Length(s) DO
+			BEGIN
+				child := GetNodeAt(cur^.child, s, s[j]);
 
-            mid := GetMem(SizeOf(TNode));
-            mid^.offset := j;
-            mid^.len := k - j;
-            mid^.next_sibling := nil;
-            mid^.child := newNode;
-            
-            cur^.child := mid;
-          END;
-        END
-        ELSE
-        BEGIN
-          child := GetMem(SizeOf(TNode));
-          child^.offset := j - 1;
-          child^.len := Length(s) - cur^.offset;
-          child^.next_sibling := cur^.child;
-          child^.child := nil;
+				IF child <> nil THEN
+				BEGIN
+					offset := child^.offset;
+					len := child^.len;
 
-          cur^.child := child;
-        END;
-      END;
-    END;
-    result.root := root;
-  END;
- 
+					k := j + 1;
+					WHILE (k - j < len) AND (s[k] = s[offset + 1 + k - j]) DO
+					BEGIN
+						Inc(k);
+					END;
+
+					IF (k - j) = len THEN
+					BEGIN
+						WRiteLn('Full traverse of: ', GetString(child^, s),'; continuing');
+						cur := child;
+						j := k;
+					END
+					ELSE
+					BEGIN
+						newNode := GetMem(SizeOf(TNode));
+						newNode^.offset := j + child^.offset;
+						newNode^.len := Length(s) - newNode^.offset;
+						newNode^.next_sibling := child;
+						newNode^.child := nil;
+
+						mid := GetMem(SizeOf(TNode));
+						mid^.offset := child^.offset;
+						mid^.len := k - j;
+						mid^.next_sibling := cur^.child;
+						mid^.child := newNode;
+
+						child^.offset := child^.offset + (k - j);
+						child^.len := child^.len - (k - j);
+
+						cur^.child := mid;
+
+						nodes := nodes + 2;
+
+						WRiteLn('Mid node: ',GetString(mid^, s));
+						WriteLN('new node: ',GetString(newNode^, s));
+						WriteLn('Updated child node: ',GetString(child^, s));
+						break;
+					END;
+				END
+				ELSE
+				BEGIN
+					child := GetMem(SizeOf(TNode));
+					child^.offset := j - 1;
+					child^.len := Length(s) - child^.offset;
+					child^.next_sibling := cur^.child;
+					child^.child := nil;
+
+					cur^.child := child;
+
+					nodes := nodes + 1;
+
+					WriteLn('new child node: ',GetString(child^, s));
+					break;
+				END;
+			END;
+		END;
+		result.root := root;
+		result.s := s;
+		result.nodes := nodes;
+	END;
+
+	FUNCTION GetString(node : TNode; s : String) : String;
+	VAR
+		i : Integer;
+		{result : String;}
+	BEGIN
+		result := '';
+		{WRiteLn('offset: ', node.offset);
+		WriteLn('length: ', node.len);
+		WriteLN('string: ', s);}
+		FOR i := node.offset + 1 TO node.offset + node.len DO
+		BEGIN
+			result := result + s[i];
+		END;
+	END;
+
+	{FUNCTION CountNodes(node : Nptr) : Integer;
+	{VAR
+		result : Integer;
+	BEGIN
+		result := 0;
+		IF node <> nil THEN
+		BEGIN
+			result := 1 + CountNodes(node^.next_sibling) + CountNodes(node^.child);
+		END;
+	END;
+
+	FUNCTION NumberLeaves(tree : TSuffixTree) : Integer;
+	{VAR
+		result : Integer;
+	BEGIN
+		result := CountNodes(Addr(tree.root));
+	END;}
+
 BEGIN
 END.
