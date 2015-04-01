9c952ca8bc999776eba60da902089ba5fa54f6e7
ThreeFx <ben@sjfiedler.de>
2015-04-01 18:18:26 +0400

    Doing some work

--- seilschaften/src/solution.hs
+++ seilschaften/src/solution.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Control.Monad (filterM)
 import Data.Function (on)
-import Data.List (sortBy)
+import Data.List ((\\), sortBy)
 import System.Environment (getArgs)
 
 -- To implement :
@@ -24,7 +24,10 @@ infixr 3 .&&.
 (.&&.) :: (a -> Bool) -> (a -> Bool) -> (a -> Bool)
 p .&&. q = \x -> p x && q x
 
-main = print . isSolvable . parse . lines =<< readFile . head =<< getArgs
+main = putStrLn . stringify . solve . parse . lines =<< readFile . head =<< getArgs
+
+stringify [] = "No possible solution"
+stringify xs = unlines $ map show xs
 
 parse :: [String] -> Problem
 parse (x:xs) = P (map (parseWeight . words) xs) (read x)
@@ -38,20 +41,35 @@ parseWeight [typ, weight, pos] = W (parse' typ) (read weight) (parse'' pos)
 		parse'' _ = Down
 parseWeight _                  = error "Wrong format"
 
-getSteps (P weights threshold) =
-        extractSteps threshold $ getWeightDifferences $ relevantWeights weights
-        	where
-        		getWeightDifferences w = zipWith (\x y -> (fst x, snd x - snd y)) (tail w) w
+solve :: Problem -> [[Weight]]
+solve problem@(P weights threshold)
+	| isSolvable = solution
+	| otherwise  = []
+		where
+			solution = flip getSteps threshold $ relevantWeights weights
+			isSolvable = all (<= threshold)
+			             $ map abs
+			             $ (\x -> zipWith (-) x (tail x))
+			             $ map getWeightOf
+			             $ map fst
+			             $ filter transportedPerson
+			             $ zip solution (tail solution)
+				where
+					transportedPerson (s1,s2) = any isPerson $ s2 \\ s1
 
-extractSteps :: Int -> [([Weight], Int)] -> [[Weight]]
-extractSteps threshold list = undefined
+getSteps :: [([Weight], Int)] -> Int -> [[Weight]]
+getSteps [] _ = []
+getSteps (state:states) threshold = fst state : (flip getSteps threshold $ dropUntilLast (valid threshold state) states)
+	where
+		valid t (state, w0) (newState, w1) = w1 - w0 <= t
+		                                     || (all isWeight $ (newState \\ state) ++ (state \\ newState))
+		                                        && filter isPerson state == filter isPerson newState
 
-isSolvable :: Problem -> Bool
-isSolvable (P w threshold) =
-        all (< threshold + getWeightBy (isWeight .&&. flip hasPerson w) w)
-        $ zipWith (-) (tail rw) rw
-        	where
-        		rw = map snd $ relevantWeights w
+dropUntilLast :: (a -> Bool) -> [a] -> [a]
+dropUntilLast p [] = []
+dropUntilLast p all@(_:xs)
+	| any p xs = dropUntilLast p xs
+	| otherwise = all
 
 -- | Returns all relevant weights including their total weight.
 -- | Relevant are all weights including the goal and the initial position.
@@ -109,3 +127,9 @@ test3 = P [W True 195 Up, W True 105 Up, W True 90 Up, W False 75 Up,
 
 test4 :: Problem
 test4 = P [W True 2 Up, W False 1 Down] 1
+
+test5 :: Problem
+test5 = P [W True 1 Up,W True 2 Up,W True 4 Up,W True 8 Up,W True 16 Up,
+           W True 32 Up,W True 64 Up,W True 128 Up,W True 256 Up] 20
+
+test6 = P [W True 10 Up, W False 20 Down] 5
