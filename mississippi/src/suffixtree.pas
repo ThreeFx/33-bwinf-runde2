3907fca2cbecd5a86169187792567349804f7a9f
ThreeFx <ben@sjfiedler.de>
2015-04-10 02:08:32 +0200

    Commenting and documentation

--- mississippi/src/suffixtree.pas
+++ mississippi/src/suffixtree.pas
@@ -45,11 +45,16 @@ IMPLEMENTATION USES SysUtils;
 	VAR
 		i : Integer;
 	BEGIN
+		// Zu Anfang ist keine Zeichenkette vorhanden
 		result := '';
+
+		// Wenn der Knoten existiert
 		IF node <> nil THEN
 		BEGIN
+			// Sollen alle Zeichen im Bereich der Kante
 			FOR i := node^.offset + 1 TO node^.offset + node^.len DO
 			BEGIN
+				// Zu einer Zeichenkette zusammengefügt werden
 				result := result + s[i];
 			END;
 		END;
@@ -67,14 +72,23 @@ IMPLEMENTATION USES SysUtils;
 
 	FUNCTION GetNodeAt(node : NodePtr; s : String; c : Char) : NodePtr;
 	BEGIN
+		// Es wird davon ausgegegangen, dass kein
+		// Knoten mit dem Buchstaben existiert
 		result := nil;
 		WHILE node <> nil DO
 		BEGIN
+			// Solange es noch Nachbarn gibt:
+
+			// Wenn der Anfangsbuchstabe der KAnte mit dem gewünschten
+			// Anfangsbuchstaben übereinstimmt
 			IF s[node^.offset + 1] = c THEN
 			BEGIN
+				// Gebe diesen Knoten zurück
 				result := node;
 				exit;
 			END;
+
+			// Ansonsten: Suche weiter
 			node := node^.next_sibling;
 		END;
 	END;
@@ -129,118 +143,113 @@ IMPLEMENTATION USES SysUtils;
 		i, j, k, len, offset, nodes : Integer;
 		root, cur, child, mid, newNode : NodePtr;
 	BEGIN
-		s := s + '$';
-		
-		WriteLn('[CreateSuffixTree] Input size: ',Length(s));
-		
+		s := s + '$'; //$ Das ist für mein Vim Syntax Highlighting
 
+		// Erstellen eines Knotens mit der Information des gesamten Strings
 		cur := GetMem(SizeOf(TNode));
 		cur^.offset := 0;
 		cur^.len := Length(s);
 		cur^.next_sibling := nil;
 		cur^.child := nil;
 
-		{
-		WriteLn('[CreateSuffixTree] Inserted full string: ',GetString(cur, s));
-		}
+		// Erstellung des Wurzelknotens
 		root := GetMem(SizeOf(TNode));
 		root^.offset := 0;
 		root^.len := 0;
 		root^.child := nil;
 		root^.next_sibling := nil;
 
+		// Anfügen des Strings an die Wurzel
 		AddChild(root, cur);
-
 		nodes := 2;
 
+		// Für alle verbleibenden Suffixe
 		FOR i := 2 TO Length(s) DO
 		BEGIN
+			// Beginne an der Wurzel
 			cur := root;
 			j := i;
 
-			{
-			WriteLn('---------------------------------------');
-			}
-
 			WHILE j <= Length(s) DO
 			BEGIN
+				// Bestimme, ob bereits eine Kante mit dem Buchstaben
+				// von dem aktuellen Knoten ausgeht
 				child := GetNodeAt(cur^.child, s, s[j]);
 
+				// Wenn dies der Fall ist
 				IF child <> nil THEN
 				BEGIN
 					offset := child^.offset;
 					len := child^.len;
 
+					// Laufe den String Schritt für Schritt solange
+					// wie er identisch mit der Kante ist ab
 					k := j + 1;
 					WHILE (k - j < len) AND (s[k] = s[offset + 1 + k - j]) DO
 					BEGIN
 						Inc(k);
 					END;
 
+					// Wenn die Kante vollständig durchlaufen wurde
 					IF (k - j) = len THEN
 					BEGIN
+						// Führe dasselbe Verfahren am nächsten Knoten durch
 						cur := child;
 						j := k;
 					END
 					ELSE
 					BEGIN
-						{
-						WriteLn('[CreateSuffixTree] Parent node: ', GetString(cur, s));
-						WriteLn('[CreateSuffixTree] Current node: ',GetString(child, s));
-						}
+						// Ansonsten sind wir mitten in der Kante abgefallen und
+						// müssen diese teilen
 
+						// Erstelle eine neue Kante mit dem neuen Rest, der noch
+						// eingefügt werden muss
 						newNode := GetMem(SizeOf(TNode));
 						newNode^.offset := k - 1;
 						newNode^.len := Length(s) - newNode^.offset;
 						newNode^.child := nil;
 						newNode^.next_sibling := nil;
 
+						// Erstelle einen neuen Knoten, an dem die Abzweigung
+						// verläuft
 						mid := GetMem(SizeOf(TNode));
 						mid^.offset := child^.offset;
 						mid^.len := k - j;
 						mid^.child := nil;
 						mid^.next_sibling := nil;
 
+						// Passe die existierende Kante an
 						child^.offset := child^.offset + (k - j);
 						child^.len := child^.len - (k - j);
 
+						// Füge den mittleren Knoten in der Mitte ein
 						ReplaceChildWith(cur, child, mid);
 
+						// Füge die beiden Suffixreste an der neu erstellten Verzweigung an
 						AddChild(mid, newNode);
 						AddChild(mid, child);
 
-						{
-						WriteLn('[CreateSuffixTree] Mid node: ', GetString(mid, s));
-						WriteLn('[CreateSuffixTree] New node: ', GetString(newNode, s));
-						WriteLn('[CreateSuffixTree] Update child node: ', GetString(child, s));
-						}
-
 						nodes := nodes + 2;
-
 						break;
 					END;
 				END
 				ELSE
 				BEGIN
+					// Wenn noch keine Kante mit dem aktuellen Buchstaben beginnt,
+					// füge eine neue Kante an dem aktuellen Knoten an
 					child := GetMem(SizeOf(TNode));
 					child^.offset := j - 1;
 					child^.len := Length(s) - child^.offset;
 					child^.child := nil;
 					child^.next_sibling := nil;
-
 					AddChild(cur, child);
 
-					{
-					WriteLn('[CreateSuffixTree] Parent node: ',GetString(cur, s));
-					WriteLn('[CreateSuffixTree] Inserted new child: ',GetString(child, s));
-					}
-
 					nodes := nodes + 1;
-
 					break;
 				END;
 			END;
 		END;
+		// Schließlich fehlt nur noch die Zweisung der Felder des Suffixbaums
 		result.root := root;
 		result.s := s;
 		result.nodes := nodes;
@@ -287,37 +296,54 @@ IMPLEMENTATION USES SysUtils;
 		END;
 	END;
 
-	FUNCTION Add(nodelist : NodeListPtr; len, rep : Integer; list : ResultListPtr; s : String) : ResultListPtr; OVERLOAD;
+	FUNCTION Add(nodelist : NodeListPtr; l, k : Integer; list : ResultListPtr; s : String) : ResultListPtr; OVERLOAD;
 	VAR
 		curNode : ResultListPtr;
 	BEGIN
+		// Das Ergebnis sei die Ursprungsliste
 		result := list;
+
+		// Wenn die einzufügende Teilkette nicht nil ist
 		IF nodelist <> nil THEN
 		BEGIN
 			curNode := list;
+
+			// Solange die Ergebnisliste nicht leer ist
 			WHILE curNode <> nil DO
 			BEGIN
-				IF curNode^.rep = rep THEN
+				// Wenn die Anzahl der Wiederholungen der einzufügenden
+				// Zeichenkette und der aktuellen gleich sind.
+				IF curNode^.rep = k THEN
 				BEGIN
+					// Wenn die aktuell betrachtete Teilkette Bestandteil der
+					// einzufügenden ist
 					IF Pos(GetStringFrom(curNode^.nodes, s), GetStringFrom(nodelist, s)) > 0 THEN
 					BEGIN
+						// Ersetze die aktuelle Teilkette mit der einzufügenden,
+						// da die einzufügende maximaler ist
 						curNode^.nodes := nodelist;
-						curNode^.len := len;
+						curNode^.len := l;
 						exit;
 					END
+					// Ansonsten wenn die einzufügende Teilkette Bestandteil
+					// der aktuell betrachtenden ist
 					ELSE IF Pos(GetStringFrom(nodelist, s), GetStringFrom(curNode^.nodes, s)) > 0 THEN
 					BEGIN
+						// Breche den Vorgang ab, da die maximale Teilkette
+						// bereits vorhanden ist
 						exit;
 					END;
 				END;
 
+				// Wiederholde dasselbe für das nächste Element
 				curNode := curNode^.next;
 			END;
 
+			// Ansonsten wurde eine neue Teilkette gefunden und eingefügt
 			result := GetMem(SizeOf(TResultList));
 			result^.nodes := nodelist;
-			result^.len := len;
-			result^.rep := rep;
+			result^.len := l;
+			result^.rep := k;
 			result^.next := list;
 		END;
 	END;
@@ -339,28 +365,33 @@ IMPLEMENTATION USES SysUtils;
 		END;
 	END;
 
-	FUNCTION FindSubstringsInNode(len, rep : Integer; ilist : NodeListPtr;
-		node : NodePtr; curLen : Integer; s : String) : ResultListPtr;
+	// Übergeben werden: Die Mindestlänge, Mindesthäufigkeit, aktueller Pfad
+	FUNCTION FindSubstringsInNode(l, k : Integer; path : NodeListPtr;
+	// Der aktuelle Knoten, die aktuelle Länge des Pfages und die Zeichenkette,
+	// aus der der Baum erzeugt wurde
+		node : NodePtr; len : Integer; s : String) : ResultListPtr;
 	BEGIN
+		// Es wird davon ausgegangen, dass keine
+		// Zeichenketten den Kriterien entsprechen
 		result := nil;
 
+		// Solange es noch Nachbarn gibt:
 		WHILE node <> nil DO
 		BEGIN
-			{
-			WriteLn('[FindSubstringsInNode] Node #',Integer(node),': ',GetString(node, s),
-				    ' has ',LeavesBelow(node),
-				    ' leaves; current length ',curLen + node^.len);
-			}
-
-			IF ((curLen + node^.len) >= len) AND (LeavesBelow(node) >= rep) THEN
+			// Wenn sie mindestens l Zeichen lang ist und k-mal vorkommt
+			IF ((len + node^.len) >= l) AND (LeavesBelow(node) >= k) THEN
 			BEGIN
-				result := Add(Add(node, ilist), node^.len + curLen, LeavesBelow(node), result, s);
+				// Füge sie dem Ergebnis hinzu
+				result := Add(Add(node, path), node^.len + len, LeavesBelow(node), result, s);
 			END;
 
+			// Füge dem Ergebnis alle gefundenen Teilzeichenketten des Kindes
+			// des aktuellen Knotens hinzu
 			result := AddCollection(
-					    FindSubstringsInNode(len, rep, Add(node, ilist), node^.child, node^.len + curlen, s),
-					    result, s);
+						FindSubstringsInNode(l, k, Add(node, path), node^.child, node^.len + len, s),
+						result, s);
 
+			// Führe dasselbe Verfahren für den nächsten Nachbarn durch
 			node := node^.next_sibling;
 		END;
 	END;
